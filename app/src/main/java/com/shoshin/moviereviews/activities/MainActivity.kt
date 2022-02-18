e40735f004e4463d97a0a63952f1c2b34a7b8699
Shoshin Oleg <shoshin1997@yandex.ru>
2022-02-18 23:04:00 +0300

    Set ReviewsFragment in MainActivity

--- app/src/main/java/com/shoshin/moviereviews/activities/MainActivity.kt
+++ app/src/main/java/com/shoshin/moviereviews/activities/MainActivity.kt
@@ -2,11 +2,24 @@ package com.shoshin.moviereviews.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.shoshin.moviereviews.R
+import com.shoshin.moviereviews.databinding.ActivityMainBinding
+import com.shoshin.moviereviews.fragments.reviews.ReviewsFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainer, ReviewsFragment())
+                .commit()
+        }
     }
 }
