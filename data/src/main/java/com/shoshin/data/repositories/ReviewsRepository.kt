c34293151d08e6c785df1e25a0a2bc134806449b
Shoshin Oleg <shoshin1997@yandex.ru>
2022-02-18 22:19:56 +0300

    Add ReviewsRepository

---
+++ data/src/main/java/com/shoshin/data/repositories/ReviewsRepository.kt
@@ -0,0 +1,25 @@
+package com.shoshin.data.repositories
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.shoshin.data.interfaces.sources.IReviewRemoteSource
+import com.shoshin.data.sources.ReviewPagingSource
+import com.shoshin.domain.entities.Review
+import com.shoshin.domain.repositories.IReviewsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ReviewsRepository @Inject constructor(
+    private val reviewRemoteSource: IReviewRemoteSource,
+): IReviewsRepository {
+    override fun getReviews(): Flow<PagingData<Review>> {
+        return Pager(
+            config = PagingConfig(
+                ReviewPagingSource.PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { ReviewPagingSource(reviewRemoteSource) }
+        ).flow
+    }
+}
